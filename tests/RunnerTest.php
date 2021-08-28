c286f6bce8fa883b3988bf2bf3d14a275e43d648
tinimi <tinimi@gmail.com>
2021-08-28 16:06:37 +0300

    Add vfs to tests

--- tests/RunnerTest.php
+++ tests/RunnerTest.php
@@ -5,29 +5,33 @@ declare(strict_types=1);
 namespace App\Tests;
 
 use App\Runner;
+use org\bovigo\vfs\vfsStream;
 use Psr\Log\LoggerInterface;
 
 class RunnerTest extends AbstractMyTestCase
 {
-    protected string $fileName = '/tmp/test.csv';
-
     public function testRunnerWithoutSort(): void
     {
-        $data = <<<QWE
-        2014-12-31,4,private,withdraw,1200.00,EUR
-        2015-01-01,4,private,withdraw,1000.00,EUR
-        2016-01-05,4,private,withdraw,1000.00,EUR
-        2016-01-05,1,private,deposit,200.00,EUR
-        2016-01-06,2,business,withdraw,300.00,EUR
-        2016-01-06,1,private,withdraw,30000,JPY
-        2016-01-07,1,private,withdraw,1000.00,EUR
-        2016-01-07,1,private,withdraw,100.00,USD
-        2016-01-10,1,private,withdraw,100.00,EUR
-        2016-01-10,2,business,deposit,10000.00,EUR
-        2016-01-10,3,private,withdraw,1000.00,EUR
-        2016-02-15,1,private,withdraw,300.00,EUR
-        2016-02-19,5,private,withdraw,3000000,JPY
-        QWE;
+        $structure = [
+            'input.csv' => <<<QWE
+                2014-12-31,4,private,withdraw,1200.00,EUR
+                2015-01-01,4,private,withdraw,1000.00,EUR
+                2016-01-05,4,private,withdraw,1000.00,EUR
+                2016-01-05,1,private,deposit,200.00,EUR
+                2016-01-06,2,business,withdraw,300.00,EUR
+                2016-01-06,1,private,withdraw,30000,JPY
+                2016-01-07,1,private,withdraw,1000.00,EUR
+                2016-01-07,1,private,withdraw,100.00,USD
+                2016-01-10,1,private,withdraw,100.00,EUR
+                2016-01-10,2,business,deposit,10000.00,EUR
+                2016-01-10,3,private,withdraw,1000.00,EUR
+                2016-02-15,1,private,withdraw,300.00,EUR
+                2016-02-19,5,private,withdraw,3000000,JPY
+                QWE
+        ];
+
+        $root = vfsStream::setup('root', null, $structure);
+
         $this->expectOutputString(<<<ASD
         0.60
         3.00
@@ -44,33 +48,37 @@ class RunnerTest extends AbstractMyTestCase
         8611
         
         ASD);
-        file_put_contents($this->fileName, $data);
 
         /**
          * @var Runner;
          */
         $runner = $this->container->get(Runner::class);
         $this->assertNotNull($runner);
-        $runner->runWithoutSort($this->fileName);
+        $runner->runWithoutSort($root->url().'/input.csv');
     }
 
     public function testRunnerWithSort(): void
     {
-        $data = <<<QWE
-        2016-02-19,5,private,withdraw,3000000,JPY
-        2014-12-31,4,private,withdraw,1200.00,EUR
-        2015-01-01,4,private,withdraw,1000.00,EUR
-        2016-01-05,4,private,withdraw,1000.00,EUR
-        2016-01-05,1,private,deposit,200.00,EUR
-        2016-01-06,2,business,withdraw,300.00,EUR
-        2016-01-07,1,private,withdraw,1000.00,EUR
-        2016-01-06,1,private,withdraw,30000,JPY
-        2016-01-07,1,private,withdraw,100.00,USD
-        2016-01-10,1,private,withdraw,100.00,EUR
-        2016-01-10,2,business,deposit,10000.00,EUR
-        2016-01-10,3,private,withdraw,1000.00,EUR
-        2016-02-15,1,private,withdraw,300.00,EUR
-        QWE;
+        $structure = [
+            'input.csv' => <<<QWE
+                2016-02-19,5,private,withdraw,3000000,JPY
+                2014-12-31,4,private,withdraw,1200.00,EUR
+                2015-01-01,4,private,withdraw,1000.00,EUR
+                2016-01-05,4,private,withdraw,1000.00,EUR
+                2016-01-05,1,private,deposit,200.00,EUR
+                2016-01-06,2,business,withdraw,300.00,EUR
+                2016-01-07,1,private,withdraw,1000.00,EUR
+                2016-01-06,1,private,withdraw,30000,JPY
+                2016-01-07,1,private,withdraw,100.00,USD
+                2016-01-10,1,private,withdraw,100.00,EUR
+                2016-01-10,2,business,deposit,10000.00,EUR
+                2016-01-10,3,private,withdraw,1000.00,EUR
+                2016-02-15,1,private,withdraw,300.00,EUR
+                QWE
+        ];
+
+        $root = vfsStream::setup('root', null, $structure);
+
         $this->expectOutputString(<<<ASD
         8611
         0.60
@@ -87,14 +95,13 @@ class RunnerTest extends AbstractMyTestCase
         0.00
         
         ASD);
-        file_put_contents($this->fileName, $data);
 
         /**
          * @var Runner;
          */
         $runner = $this->container->get(Runner::class);
         $this->assertNotNull($runner);
-        $runner->runWithSort($this->fileName);
+        $runner->runWithSort($root->url().'/input.csv');
     }
 
     public function testRun1(): void
--- tests/Service/Reader/CSVReaderTest.php
+++ tests/Service/Reader/CSVReaderTest.php
@@ -4,40 +4,61 @@ declare(strict_types=1);
 
 namespace App\Tests\Service;
 
+use App\DTO\CurrencyDTO;
 use App\DTO\TransactionDTO;
 use App\Exceptions\FileNotFoundException;
 use App\Factory\TransactionFactoryInterface;
 use App\Service\Reader\CSVReader;
-use App\Tests\AbstractMyTestCase;
+use DateTimeImmutable;
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
 
-class CSVReaderTest extends AbstractMyTestCase
+class CSVReaderTest extends TestCase
 {
     public function testException(): void
     {
         $this->expectException(FileNotFoundException::class);
 
-        /**
-         * @var TransactionFactoryInterface
-         */
-        $transactionFactory = $this->container->get('TransactionFactory');
-        $this->assertNotNull($transactionFactory);
+        $transaction = new TransactionDTO(
+            $date = new DateTimeImmutable('2020-01-02'),
+            '123',
+            'private',
+            'deposit',
+            '1200',
+            $currency = new CurrencyDTO('USD', 2)
+        );
+
+        $transactionFactory = $this->createStub(TransactionFactoryInterface::class);
+        $transactionFactory->method('createFromArray')->willReturn($transaction);
+
         $reader = new CSVReader($transactionFactory);
-        $reader->setFileName('/tmp/qwe');
+        $reader->setFileName('/tmp/qwe/asdf/zxcv');
         foreach ($reader->getTransaction() as $t) {
         }
     }
 
     public function testOk(): void
     {
-        file_put_contents('/tmp/qwe', '2014-12-31,4,private,withdraw,1200.00,EUR');
+        $structure = [
+            'input.csv' => '2014-12-31,4,private,withdraw,1200.00,EUR',
+        ];
 
-        /**
-         * @var TransactionFactoryInterface
-         */
-        $transactionFactory = $this->container->get('TransactionFactory');
-        $this->assertNotNull($transactionFactory);
+        $root = vfsStream::setup('root', null, $structure);
+
+        $transaction = new TransactionDTO(
+            $date = new DateTimeImmutable('2020-01-02'),
+            '123',
+            'private',
+            'deposit',
+            '1200',
+            $currency = new CurrencyDTO('USD', 2)
+        );
+
+        $transactionFactory = $this->createStub(TransactionFactoryInterface::class);
+        $transactionFactory->method('createFromArray')->willReturn($transaction);
+
         $reader = new CSVReader($transactionFactory);
-        $reader->setFileName('/tmp/qwe');
+        $reader->setFileName($root->url().'/input.csv');
         foreach ($reader->getTransaction() as $transaction) {
             $this->assertInstanceOf(TransactionDTO::class, $transaction);
         }
